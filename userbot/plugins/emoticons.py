6161453c32c73ffd4cd1ee12a57a6c0b01ba3f22
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-14 21:43:13 +0530

    Create emoticons.py

---
+++ userbot/plugins/emoticons.py
@@ -0,0 +1,130 @@
+import base64
+from asyncio import sleep
+
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
+from .. import legend
+from ..core.logger import logging
+from ..core.managers import eod, eor
+from ..helpers.utils import _format, get_user_from_event
+from ..sql_helper import broadcast_sql as sql
+from . import BOTLOG, BOTLOG_CHATID
+
+menu_category = "tools"
+
+LOGS = logging.getLogger(__name__)
+
+
+@legend.legend_cmd(
+    pattern="swt(?:\s|$)([\s\S]*)",
+    command=("swt", menu_category),
+    info={
+        "header": "Try It Your Self.",
+        "description": "Random Flags",
+        "flags": {
+            "ex": "To Send Message In All User & Group",
+            "think": "To Send Message In All Group",
+            "p": "To Send Message In All User",
+        },
+        "usage": [
+            "{tr}swt <flags>",
+        ],
+        "examples": [
+            "{tr}swt ex",
+        ],
+    },
+)
+async def _(event):
+    input_str = event.pattern_match.group(1)
+    if input_str in "ex":
+        emoticons = [
+            "u is mard",
+            "u is man",
+            "u is aurat",
+            "u is woman",
+            "u is gey",
+            "u is chakka",
+        ]
+    elif input_str in "thinkings":
+        emoticons = [
+            "(҂⌣̀_⌣́)",
+            "（；¬＿¬)",
+            "(-｡-;",
+            "┌[ O ʖ̯ O ]┐",
+            "〳 ͡° Ĺ̯ ͡° 〵",
+        ]
+    elif input_str in "waving":
+        emoticons = [
+            "(ノ^∇^)",
+            "(;-_-)/",
+            "@(o・ェ・)@ノ",
+            "ヾ(＾-＾)ノ",
+            "ヾ(◍’౪◍)ﾉﾞ♡",
+            "(ό‿ὸ)ﾉ",
+            "(ヾ(´・ω・｀)",
+        ]
+    elif input_str in "wtfs":
+        emoticons = [
+            "༎ຶ‿༎ຶ",
+            "(‿ˠ‿)",
+            "╰U╯☜(◉ɷ◉ )",
+            "(;´༎ຶ益༎ຶ)♡",
+            "╭∩╮(︶ε︶*)chu",
+            "( ＾◡＾)っ (‿|‿)",
+        ]
+    elif input_str in "loves":
+        emoticons = [
+            "乂❤‿❤乂",
+            "(｡♥‿♥｡)",
+            "( ͡~ ͜ʖ ͡°)",
+            "໒( ♥ ◡ ♥ )७",
+            "༼♥ل͜♥༽",
+        ]
+    elif input_str in "confuses":
+        emoticons = [
+            "(・_・ヾ",
+            "｢(ﾟﾍﾟ)",
+            "﴾͡๏̯͡๏﴿",
+            "(￣■￣;)!?",
+            "▐ ˵ ͠° (oo) °͠ ˵ ▐",
+            "(-_-)ゞ゛",
+        ]
+    elif input_str in "dead":
+        emoticons = [
+            "(✖╭╮✖)",
+            "✖‿✖",
+            "(+_+)",
+            "(✖﹏✖)",
+            "∑(✘Д✘๑)",
+        ]
+    elif input_str in "sad":
+        emoticons = [
+            "(＠´＿｀＠)",
+            "⊙︿⊙",
+            "(▰˘︹˘▰)",
+            "●︿●",
+            "(　´_ﾉ` )",
+            "彡(-_-;)彡",
+        ]
+    elif input_str in "dogs":
+        emoticons = [
+            "-ᄒᴥᄒ-",
+            "◖⚆ᴥ⚆◗",
+        ]
+    else:
+        emoticons = [
+            "( ͡° ͜ʖ ͡°)",
+            "¯\_(ツ)_/¯",
+            "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)",
+            "ʕ•ᴥ•ʔ",
+            "(▀ Ĺ̯▀   )",
+            "(ง ͠° ͟ل͜ ͡°)ง",
+            "༼ つ ◕_◕ ༽つ",
+            "ಠ_ಠ",
+            "(☞ ͡° ͜ʖ ͡°)☞",
+            "¯\_༼ ି ~ ି ༽_/¯",
+            "c༼ ͡° ͜ʖ ͡° ༽⊃",
+        ]
+    index = random.randint(0, len(emoticons))
+    output_str = emoticons[index]
+    await event.edit(output_str)
