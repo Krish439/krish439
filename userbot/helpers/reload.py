cbc591b604d75f2b514fbaad3b039da68cf67cf8
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-20 23:11:18 +0530

    Delete reload.py

--- userbot/helpers/reload.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-
-async def reload_LEGENDBOT():
-    executable = sys.executable.replace(" ", "\\ ")
-    args = [executable, "-m", "userbot"]
-    os.execle(executable, *args, os.environ)
-    os._exit(143)
