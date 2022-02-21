b1963550ffa23538a25efb37c1a1a5054f1de5fd
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-21 12:35:40 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -43,7 +43,7 @@ async def _(event):
         await event.client.send_file(
             event.chat_id,
             sweetie,
-            caption=f"<b><i>**👨‍💻 Pong** </b></i>\n➥ {ms} <b><i>ms♡\n➥ Bot of {hmention} ♡</b></i>",
+            caption=f"<b><i>👨‍💻 Pong </b></i>\n   ➡️ {ms} <b><i>ms\n   Bot : {hmention}</b></i>",
             parse_mode="html",
         )
 
