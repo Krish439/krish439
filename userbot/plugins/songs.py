1b312d8eb99a8859053f9f949d568a4ae1f4cf88
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-21 22:28:28 +0530

    Update songs.py

--- userbot/plugins/songs.py
+++ userbot/plugins/songs.py
@@ -2,6 +2,12 @@ import asyncio
 import io
 import os
 import time
+import os
+import requests
+import aiohttp
+import yt_dlp
+
+from youtube_search import YoutubeSearch
 
 import requests
 from ShazamAPI import Shazam
@@ -213,6 +219,52 @@ async def _(event):
     )
     await event.delete()
     os.remove(f"{hell_data['id']}.mp4")
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+
+
+async def shazamcmd(event):
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    m = await eor(event, "searching song")
+    query = event.text[7:]
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
+
+
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
+
+    except Exception as e:
+        m.edit(
+            "𝐒𝐨𝐧𝐠 🥀 𝐍𝐨𝐭 😔 𝐅𝐨𝐮𝐧𝐝."
+        )
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        await event.client.send_file(
+             event.chat_id,
+             audio_file,
+             caption="Song",
+             thumb=thumb_name
+             )
+
 
 
 @legend.legend_cmd(
