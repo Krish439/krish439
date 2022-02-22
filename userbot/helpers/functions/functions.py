4f9333a78f8525c09b2c1c50678fe5020bf632ff
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-22 15:51:07 +0530

    Update functions.py

--- userbot/helpers/functions/functions.py
+++ userbot/helpers/functions/functions.py
@@ -100,7 +100,7 @@ async def animator(media, mainevent, textevent):
     LegendOp = await mainevent.client.download_media(media, Config.TEMP_DIR)
     await textevent.edit("__🎞Converting into Animated sticker..__")
     await runcmd(
-        f"ffmpeg -ss 00:00:00 -to 00:00:02.900 -i {BadCat} -vf scale={w}:{h} -c:v libvpx-vp9 -crf 30 -b:v 560k -maxrate 560k -bufsize 256k -an animate.webm"
+        f"ffmpeg -ss 00:00:00 -to 00:00:02.900 -i {LegendOp} -vf scale={w}:{h} -c:v libvpx-vp9 -crf 30 -b:v 560k -maxrate 560k -bufsize 256k -an animate.webm"
     )  # pain
     os.remove(LegendOp)
     sticker = "animate.webm"
