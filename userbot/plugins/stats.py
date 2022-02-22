ae7c07d7e9798487d36da3f269c8f220f04a2016
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-22 08:52:50 +0530

    Update stats.py

--- userbot/plugins/stats.py
+++ userbot/plugins/stats.py
@@ -98,23 +98,23 @@ async def stats(event):  # sourcery no-metrics
         unread += dialog.unread_count
     stop_time = time.time() - start_time
     full_name = inline_mention(await event.client.get_me())
-    response = f"📌 **Stats for {full_name}** \n\n"
+    response = f"📜 **Stats for {full_name}** \n\n"
     response += f"**Private Chats:** {private_chats} \n"
-    response += f"   ★ `Users: {private_chats - bots}` \n"
-    response += f"   ★ `Bots: {bots}` \n"
+    response += f"   • **Users:** `{private_chats - bots}` \n"
+    response += f"   • **Bots:** `{bots}` \n"
     response += f"**Groups:** {groups} \n"
     response += f"**Channels:** {broadcast_channels} \n"
     response += f"**Admin in Groups:** {admin_in_groups} \n"
-    response += f"   ★ `Creator: {creator_in_groups}` \n"
-    response += f"   ★ `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
+    response += f"   ▪ **Creator:** `{creator_in_groups}` \n"
+    response += f"   ▪ **Admin Rights:** `{admin_in_groups - creator_in_groups}` \n"
     response += f"**Admin in Channels:** {admin_in_broadcast_channels} \n"
-    response += f"   ★ `Creator: {creator_in_channels}` \n"
+    response += f"   ♡ **Creator:** `{creator_in_channels}` \n"
     response += (
-        f"   ★ `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n"
+        f"   ★ **Admin Rights:** `{admin_in_broadcast_channels - creator_in_channels}` \n"
     )
     response += f"**Unread:** {unread} \n"
     response += f"**Unread Mentions:** {unread_mentions} \n\n"
-    response += f"📌 __It Took:__ {stop_time:.02f}s \n"
+    response += f"🚩 __It Took:__ {stop_time:.02f}s \n"
     await legend.edit(response)
 
 
