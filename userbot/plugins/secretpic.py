0b5d5e501f87e4bc7d4d71b61df4b2c7fbd3c497
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-22 21:26:08 +0530

    Create secretpic.py

---
+++ userbot/plugins/secretpic.py
@@ -0,0 +1,30 @@
+from ..Config import Config
+from ..core.managers import eor
+from ..sql_helper.globals import gvarstatus
+from . import _legendtools, convert_toimage, deEmojify, legend
+
+
+
+@legend.legend_cmd(
+    pattern="sdpic(?:\s|$)([\s\S]*)",
+    command=("sdpic", menu_category),
+    info={
+        "header": "This Command Can Capture The Self Destruction Picture",
+        "usage": "{tr}sdpic <reply to self distruct pic>",
+        "examples": "{tr}sdpic",
+    },
+)
+async def oho(event):
+    if not event.is_reply:
+        return await event.edit("Reply to a self distructing pic !.!.!")
+    k = await event.get_reply_message()
+    pic = await k.download_media()
+    await event.client.send_file(
+        event.chat_id,
+        pic,
+        caption=f"""
+OwO!! LoL, Secret Pic Mode Pic Destroyed!!
+Pic captured By Lêɠêɳ̃dẞø†
+  """,
+    )
+    await event.delete()
