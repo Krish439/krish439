8566c9ed1a11f598fa9a0190a4fd276181557e81
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-22 18:58:12 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,7 +1,7 @@
 import sys
 
 import userbot
-from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+from userbot import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
 
 from .Config import Config
 from .core.logger import logging
@@ -10,7 +10,6 @@ from .start import killer, legends
 from .utils import (
     add_bot_to_logger_group,
     hekp,
-    ipchange,
     load_plugins,
     setup_bot,
     spams,
@@ -35,19 +34,7 @@ except Exception as e:
     sys.exit()
 
 
-class LegendCheck:
-    def __init__(self):
-        self.sucess = True
-
-
-Legendcheck = LegendCheck()
-
-
 async def startup_process():
-    check = await ipchange()
-    if check is not None:
-        Legendcheck.sucess = False
-        return
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
@@ -63,7 +50,6 @@ async def startup_process():
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
     await legends()
-    Legendcheck.sucess = True
     return
 
 
@@ -72,9 +58,6 @@ legend.loop.create_task(hekp())
 
 if len(sys.argv) not in (1, 3, 4):
     legend.disconnect()
-elif not Legendcheck.sucess:
-    if HEROKU_APP is not None:
-        HEROKU_APP.restart()
 else:
     try:
         legend.run_until_disconnected()
