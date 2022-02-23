05ad3debc999b644c8621f8510180eb2757f09fc
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-23 07:01:08 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -64,7 +64,7 @@ async def gen_chlog(repo, diff):
 
 async def print_changelogs(event, ac_br, changelog):
     changelog_str = (
-        f"**New UPDATE available for [{ac_br}]:\n\nCHANGELOG:**\n`{changelog}`"
+        f"**🤖 New Update available for [{ac_br}]:\n\nChangeLogs:**\n`{changelog}`"
     )
     if len(changelog_str) > 4096:
         await event.edit("`Changelog is too big, view the file to see it.`")
