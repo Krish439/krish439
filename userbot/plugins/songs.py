f611e0850b99f1ba03cf72f78f7d3b2a8cf2c91e
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-26 00:50:10 +0530

    Update songs.py

--- userbot/plugins/songs.py
+++ userbot/plugins/songs.py
@@ -245,6 +245,55 @@ def time_to_seconds(time):
 
 
 @legend.legend_cmd(
+    pattern="tsong(?:\s|$)([\s\S]*)",
+    command=("tsong", menu_category),
+    info={
+        "header": "Search Song",
+        "usage": "{tr}tsong",
+    },
+)
+async def shazamcmd(event):
+    ydl_opts = {"format": "bestvideo[ext=mp4]"}
+    m = await eor(event, "searching song")
+    query = event.text[6:]
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"thumb{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+        views = results[0]["views"]
+
+    except Exception:
+        m.edit("𝐒𝐨𝐧𝐠 🥀 𝐍𝐨𝐭 😔 𝐅𝐨𝐮𝐧𝐝.")
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
+    except Exception as e:
+        m.edit("**𝐘𝐨𝐮𝐭𝐮𝐛𝐞  𝐄𝐫𝐫𝐨𝐫 ❌**")
+        print(e)
+    await event.client.send_file(
+        event.chat_id,
+        audio_file,
+        supports_streaming=True,
+        caption=f"**✘ Song -** `{title}` \n**✘ Views -** `{views}` \n**✘ Duration -** `{duration}` \n\n**✘ By :** {mention}",
+        thumb=thumb_name,
+    )
+    await event.delete()
+    os.remove(audio_file)
+    os.remove(thumb_name)
+
+
+@legend.legend_cmd(
     pattern="vsong(?:\s|$)([\s\S]*)",
     command=("vsong", menu_category),
     info={
