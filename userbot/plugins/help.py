bec453ff05d12c3e65a5a08adf680bc7dd7e1faf
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-25 14:23:36 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -1,7 +1,7 @@
 from telethon import functions
 
 from userbot import legend
-
+from ..sql_helper.globals import gvarstatus, addgvar, delgvar
 from ..Config import Config
 from ..core import CMD_INFO, GRP_INFO, PLG_INFO
 from ..core.managers import eod, eor
