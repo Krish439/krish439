511ce4192b921c268c188322471fb77395ef9962
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-26 20:19:22 +0530

    Update botcontrols.py

--- userbot/assistant/botcontrols.py
+++ userbot/assistant/botcontrols.py
@@ -31,20 +31,20 @@ cmhd = Config.HANDLER
 @legend.bot_cmd(pattern="^/help$", from_users=Config.OWNER_ID)
 async def bot_help(event):
     await event.reply(
-        f"""The commands in the bot are:
-**Note : **__This commands work only in this bot__ {botusername}
-• **Cmd : **/uinfo <reply to user message>
-• **Info : **__You have noticed that forwarded stickers/emoji doesn't have forward tag so you can identify the user who sent thoose messages by this cmd.__
-• **Note : **__It works for all forwarded messages. even for users who's permission forward message nobody.__
-• **Cmd : **/ban <reason> or /ban <username/userid> <reason>
-• **Info : **__Reply to a user message with reason so he will be notified as you banned from the bot and his messages will not be forworded to you further.__
-• **Note : **__Reason is must. without reason it won't work. __
-• **Cmd : **/unban <reason(optional)> or /unban <username/userid>
-• **Info : **__Reply to user message or provide username/userid to unban from the bot.__
-• **Note : **__To check banned users list use__ `{cmhd}bblist`.
-• **Cmd : **/broadcast
-• **Info : **__Reply to a message to get broadcasted to every user who started your bot. To get list of users use__ `{cmhd}bot_users`.
-• **Note : **__if user stoped/blocked the bot then he will be removed from your database that is he will erased from the bot_starters list.__
+        f"""बोट में कमांड है।:
+**नोट : **__यह कमांड सिर्फ इसी बोट में काम करेगा__ {botusername}
+• **कमांड : **/uinfo <किसी यूजर को रिप्लाई करके>
+• **इंफॉर्मेशन : **__आपने देखा है कि फॉरवर्ड किए गए स्टिकर्स/इमोजी में फॉरवर्ड टैग नहीं होता है, इसलिए आप उस यूजर की पहचान कर सकते हैं जिसने इस कमांड द्वारा मैसेज भेजे हैं।.__
+• **नोट : **__यह सभी फॉरवर्ड किए गए संदेशों के लिए काम करता है। यहां तक ​​​​कि उन उपयोगकर्ताओं के लिए भी जिनकी अनुमति है संदेश अग्रेषित करें कोई नहीं.__
+• **कमांड : **/ban <कारण> or /ban <यूजरनेम/यूजर आईडी> <कारण>
+• **इंफॉर्मेशन : **__एक उपयोगकर्ता संदेश का कारण के साथ उत्तर दें ताकि उसे सूचित किया जाएगा क्योंकि आपने बॉट से प्रतिबंधित कर दिया है और उसके संदेश आपको आगे नहीं भेजे जाएंगे.__
+• **नोट : **__कारण जरूरी है। अकारण यह काम नहीं करेगा. __
+• **कमांड : **/unban <कारण(ऐच्छिक)> or /unban <यूजरनेम/यूजर आईडी>
+• **इंफॉर्मेशन : **__उपयोगकर्ता संदेश का उत्तर दें या बॉट से प्रतिबंध हटाने के लिए उपयोगकर्ता नाम/उपयोगकर्ता आईडी प्रदान करें.__
+• **नोट : **__प्रतिबंधित उपयोगकर्ताओं की सूची उपयोग की जांच करने के लिए__ `{cmhd}bblist`.
+• **कमांड : **/broadcast
+• **इंफॉर्मेशन : **__आपके बॉट को शुरू करने वाले प्रत्येक उपयोगकर्ता को प्रसारित करने के लिए एक संदेश का उत्तर दें। उपयोगकर्ताओं की सूची प्राप्त करने के लिए उपयोग करें__ `{cmhd}bot_users`.
+• **नोट: **__यदि उपयोगकर्ता ने बॉट को रोक दिया/अवरुद्ध कर दिया तो उसे आपके डेटाबेस से हटा दिया जाएगा यानी वह bot_starters सूची से मिटा दिया जाएगा.__
 """
     )
 
@@ -55,19 +55,19 @@ async def bot_broadcast(event):
     if not replied:
         return await event.reply("Reply to a message for Broadcasting First !")
     start_ = datetime.now()
-    br_cast = await replied.reply("Broadcasting ...")
+    br_cast = await replied.reply("ब्रॉडकास्टिंग।..")
     blocked_users = []
     count = 0
     bot_users_count = len(get_all_starters())
     if bot_users_count == 0:
-        return await event.reply("`No one started your bot yet.`")
+        return await event.reply("`किसी ने आपका बॉट शुरू नहीं किया.`")
     users = get_all_starters()
     if users is None:
-        return await event.reply("`Errors ocured while fetching users list.`")
+        return await event.reply("`उपयोगकर्ताओं की सूची लाते समय त्रुटियां हुईं.`")
     for user in users:
         try:
             await event.client.send_message(
-                int(user.user_id), "🔊 You received a **new** Broadcast."
+                int(user.user_id), "🔊 आपको एक **नया** प्रसारण प्राप्त हुआ."
             )
             await event.client.send_message(int(user.user_id), replied)
             await asyncio.sleep(0.8)
@@ -79,31 +79,30 @@ async def bot_broadcast(event):
             LOGS.error(str(e))
             if BOTLOG:
                 await event.client.send_message(
-                    BOTLOG_CHATID, f"**Error while broadcasting**\n`{e}`"
+                    BOTLOG_CHATID, f"**प्रसारण करते समय त्रुटि**\n`{e}`"
                 )
-
         else:
             count += 1
             if count % 5 == 0:
                 try:
                     prog_ = (
-                        "🔊 Broadcasting ...\n\n"
+                        "🔊 ब्रॉडकास्टिंग ...\n\n"
                         + progress_str(
                             total=bot_users_count,
                             current=count + len(blocked_users),
                         )
-                        + f"\n\n• ✔️ **Success** :  `{count}`\n"
-                        + f"• ✖️ **Failed** :  `{len(blocked_users)}`"
+                        + f"\n\n• ✔️ **सफलता** :  `{count}`\n"
+                        + f"• ✖️ **फेल्ड** :  `{len(blocked_users)}`"
                     )
                     await br_cast.edit(prog_)
                 except FloodWaitError as e:
                     await asyncio.sleep(e.seconds)
     end_ = datetime.now()
-    b_info = f"🔊  Successfully broadcasted message to ➜  <b>{count} users.</b>"
+    b_info = f"🔊संदेश को सफलतापूर्वक प्रसारित किया गया ➜  <b>{count} यूजर्स.</b>"
     if len(blocked_users) != 0:
-        b_info += f"\n🚫  <b>{len(blocked_users)} users</b> blocked your bot recently, so have been removed."
+        b_info += f"\n🚫  <b>{len(blocked_users)} यूजर्स</b> आपके बॉट को हाल ही में ब्लॉक किया है, इसलिए हटा दिया गया है।."
     b_info += (
-        f"\n⏳  <code>Process took: {time_formatter((end_ - start_).seconds)}</code>."
+        f"\n⏳  <code>प्रक्रिया ली: {time_formatter((end_ - start_).seconds)}</code>."
     )
     await br_cast.edit(b_info, parse_mode="html")
 
@@ -112,8 +111,8 @@ async def bot_broadcast(event):
     pattern="bot_users$",
     command=("bot_users", menu_category),
     info={
-        "header": "To get users list who started bot.",
-        "description": "To get compelete list of users who started your bot",
+        "header": "उपयोगकर्ताओं की सूची प्राप्त करने के लिए जिन्होंने बॉट शुरू किया.",
+        "description": "आपका बॉट शुरू करने वाले उपयोगकर्ताओं की पूरी सूची प्राप्त करने के लिए",
         "usage": "{tr}bot_users",
     },
 )
@@ -121,10 +120,10 @@ async def ban_starters(event):
     "To get list of users who started bot."
     ulist = get_all_starters()
     if len(ulist) == 0:
-        return await eod(event, "`No one started your bot yet.`")
-    msg = "**The list of users who started your bot are :\n\n**"
+        return await eod(event, "`अभी तक किसी ने आपका बॉट शुरू नहीं किया.`")
+    msg = "**आपके बॉट को शुरू करने वाले उपयोगकर्ताओं की सूची है :\n\n**"
     for user in ulist:
-        msg += f"• 👤 {_format.mentionuser(user.first_name , user.user_id)}\n**ID:** `{user.user_id}`\n**UserName:** @{user.username}\n**Date: **__{user.date}__\n\n"
+        msg += f"• 👤 {_format.mentionuser(user.first_name , user.user_id)}\n**ID:** `{user.user_id}`\n**यूजरनेम:** @{user.username}\n**तारीख: **__{user.date}__\n\n"
     await eor(event, msg)
 
 
@@ -134,27 +133,27 @@ async def ban_botpms(event):
     reply_to = await reply_id(event)
     if not user_id:
         return await event.client.send_message(
-            event.chat_id, "`I can't find user to ban`", reply_to=reply_to
+            event.chat_id, "`मुझे प्रतिबंध लगाने के लिए उपयोगकर्ता नहीं मिल रहा है .`", reply_to=reply_to
         )
     if not reason:
         return await event.client.send_message(
-            event.chat_id, "`To ban the user provide reason first`", reply_to=reply_to
+            event.chat_id, "`उपयोगकर्ता को प्रतिबंधित करने के लिए पहले कारण बताएं`", reply_to=reply_to
         )
     try:
         user = await event.client.get_entity(user_id)
         user_id = user.id
     except Exception as e:
-        return await event.reply(f"**Error:**\n`{e}`")
+        return await event.reply(f"**एरर:**\n`{e}`")
     if user_id == Config.OWNER_ID:
-        return await event.reply("I can't ban you master")
+        return await event.reply("मैं आप पर प्रतिबंध नहीं लगा सकता मास्टर")
     check = check_is_black_list(user.id)
     if check:
         return await event.client.send_message(
             event.chat_id,
-            f"#Already_banned\
-            \nUser already exists in my Banned Users list.\
-            \n**Reason For Bot BAN:** `{check.reason}`\
-            \n**Date:** `{check.date}`.",
+            f"#बैन\
+            \nउपयोगकर्ता मेरी प्रतिबंधित उपयोगकर्ता सूची में पहले से मौजूद है.\
+            \n**बैन का कारण:** `{check.reason}`\
+            \n**तारीख:** `{check.date}`.",
         )
     msg = await ban_user_from_bot(user, reason, reply_to)
     await event.reply(msg)
@@ -166,13 +165,13 @@ async def ban_botpms(event):
     reply_to = await reply_id(event)
     if not user_id:
         return await event.client.send_message(
-            event.chat_id, "`I can't find user to unban`", reply_to=reply_to
+            event.chat_id, "`मुझे प्रतिबंध हटाने के लिए उपयोगकर्ता नहीं मिल रहा है`", reply_to=reply_to
         )
     try:
         user = await event.client.get_entity(user_id)
         user_id = user.id
     except Exception as e:
-        return await event.reply(f"**Error:**\n`{e}`")
+        return await event.reply(f"**एरर:**\n`{e}`")
     check = check_is_black_list(user.id)
     if not check:
         return await event.client.send_message(
@@ -188,19 +187,19 @@ async def ban_botpms(event):
     pattern="bblist$",
     command=("bblist", menu_category),
     info={
-        "header": "To get users list who are banned in bot.",
-        "description": "To get list of users who are banned in bot.",
+        "header": "उन उपयोगकर्ताओं की सूची प्राप्त करने के लिए जिन्हें बोट में प्रतिबंधित किया गया है.",
+        "description": "में प्रतिबंधित उपयोगकर्ताओं की सूची प्राप्त करने के लिए",
         "usage": "{tr}bblist",
     },
 )
 async def ban_starters(event):
-    "To get list of users who are banned in bot."
+    "में प्रतिबंधित उपयोगकर्ताओं की सूची प्राप्त करने के लिए."
     ulist = get_all_bl_users()
     if len(ulist) == 0:
-        return await eod(event, "`No one is banned in your bot yet.`")
-    msg = "**The list of users who are banned in your bot are :\n\n**"
+        return await eod(event, "`आपके बॉट में अभी तक किसी को भी प्रतिबंधित नहीं किया गया है.`")
+    msg = "**आपके बॉट में प्रतिबंधित उपयोगकर्ताओं की सूची है:\n\n**"
     for user in ulist:
-        msg += f"• 👤 {_format.mentionuser(user.first_name , user.chat_id)}\n**ID:** `{user.chat_id}`\n**UserName:** @{user.username}\n**Date: **__{user.date}__\n**Reason:** __{user.reason}__\n\n"
+        msg += f"• 👤 {_format.mentionuser(user.first_name , user.chat_id)}\n**आईडी:** `{user.chat_id}`\n**यूजरनेम:** @{user.username}\n**तारीख: **__{user.date}__\n**कारण:** __{user.reason}__\n\n"
     await eor(event, msg)
 
 
@@ -208,8 +207,8 @@ async def ban_starters(event):
     pattern="bot_antif (on|off)$",
     command=("bot_antif", menu_category),
     info={
-        "header": "To enable or disable bot antiflood.",
-        "description": "if it was turned on then after 10 messages or 10 edits of same messages in less time then your bot auto loacks them.",
+        "header": "बॉट एंटीफ्लड को सक्षम या अक्षम करने के लिए.",
+        "description": "अगर इसे चालू किया गया था तो कम समय में 10 संदेशों या समान संदेशों के 10 संपादन के बाद आपका बॉट ऑटो उन्हें लॉक कर देता है.",
         "usage": [
             "{tr}bot_antif on",
             "{tr}bot_antif off",
@@ -217,15 +216,15 @@ async def ban_starters(event):
     },
 )
 async def ban_antiflood(event):
-    "To enable or disable bot antiflood."
+    "बॉट एंटीफ्लड को सक्षम या अक्षम करने के लिए."
     input_str = event.pattern_match.group(1)
     if input_str == "on":
         if gvarstatus("bot_antif") is not None:
-            return await eod(event, "`Bot Antiflood was already enabled.`")
+            return await eod(event, "`बॉट एंटीफ्लड पहले से ही सक्षम था।`")
         addgvar("bot_antif", True)
-        await eod(event, "`Bot Antiflood Enabled.`")
+        await eod(event, "`बॉट एंटीफ्लड सक्षम.`")
     elif input_str == "off":
         if gvarstatus("bot_antif") is None:
-            return await eod(event, "`Bot Antiflood was already disabled.`")
+            return await eod(event, "`बॉट एंटिफ्लूड पहले से ही अक्षम था.`")
         delgvar("bot_antif")
-        await eod(event, "`Bot Antiflood Disabled.`")
+        await eod(event, "`बॉट एंटीफ्लूड डिसेबल्ड.`")
