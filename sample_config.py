f5861c490e31ac15f9f517b0ab1607b49ff3a0a6
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-02-28 14:58:20 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -15,7 +15,6 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH") or None
     # Datbase url heroku sets it automatically else get this from elephantsql
     DB_URI = os.environ.get("DATABASE_URL", None)
-    # Get this value by running python3 stringsetup.py or https://repl.it/@sandeep1709/generatestringsession
     LEGEND_STRING = os.environ.get("LEGEND_STRING", None)
     # Telegram BOT Token and bot username from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN") or os.environ.get("BOT_TOKEN_BF_HER", None)
